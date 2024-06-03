c71fa5d481f1c0042b06938c0216f12a245b0b1f
diomondus <diomondus@mail.ru>
2024-06-03 22:20:41 +0200

    add cache

--- iRepeater/AudioService.swift
+++ iRepeater/AudioService.swift
@@ -10,6 +10,8 @@ import AVFoundation
 
 class AudioService {
     
+    private let audioCache = AudioCacheManager()
+    
     func playSentence(_ orig: String) {
         orig.split(separator: " ")
             .filter { word in word.count > 2 && !word.contains("(") && !word.contains(")") }
@@ -20,13 +22,19 @@ class AudioService {
     func playAudioFromURL(url: URL) {
 //        print(url)
         DispatchQueue.global().async {
-            do {
-                let data = try Data(contentsOf: url)
-                let player = try AVAudioPlayer(data: data)
-                player.play()
-                sleep(2) // yebanie pidarasi iz epol
-            } catch {
-                print("Error playing audio: \(error.localizedDescription)")
+            self.audioCache.getOrDownload(from: url) { localUrl in
+                guard let localUrl = localUrl else {
+                    print("Failed to download audio")
+                    return
+                }
+                do {
+                    let audioPlayer = try AVAudioPlayer(contentsOf: localUrl)
+//                    audioPlayer.prepareToPlay()
+                    audioPlayer.play()
+                    sleep(2) // yebanie pidarasi iz epol
+                } catch {
+                    print("Error playing audio: \(error.localizedDescription)")
+                }
             }
         }
     }
@@ -34,5 +42,37 @@ class AudioService {
     func getPronunciationServiceUrl(word: String) -> String {
 //        print(word)
         return "https://ssl.gstatic.com/dictionary/static/pronunciation/2022-03-02/audio/\(word.prefix(2))/\(word)_en_us_1.mp3"
+    }
+}
+
+class AudioCacheManager {
+
+    private let fileManager = FileManager.default
+    
+    func getOrDownload(from url: URL, completion: @escaping (URL?) -> Void) {
+        let cacheDirectory = fileManager.urls(for: .cachesDirectory, in: .userDomainMask).first!
+        let localUrl = cacheDirectory.appendingPathComponent(url.lastPathComponent)
+        
+        if fileManager.fileExists(atPath: localUrl.path) {
+            completion(localUrl)
+//            print("Аудио уже закешировано")
+        } else {
+//            print("Загрузка аудио")
+            let task = URLSession.shared.downloadTask(with: url) { (tempUrl, response, error) in
+                guard let tempUrl = tempUrl, error == nil else {
+                    completion(nil)
+                    return
+                }
+                
+                do {
+                    try self.fileManager.moveItem(at: tempUrl, to: localUrl)
+                    completion(localUrl)
+                } catch {
+                    print("Ошибка при перемещении файла: \(error)")
+                    completion(nil)
+                }
+            }
+            task.resume()
+        }
     }
 }
